c5d3cb981238b3fd5e1951c89cc45bcccab97a19
Dave <daveg7lee@gmail.com>
2021-04-19 20:28:22 +0900

    ADD Default Avatar

---
+++ prisma/migrations/20210417073935_default_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "avatar" SET DEFAULT E'https://res.cloudinary.com/du4erd9mp/image/upload/v1609642686/User%20Profile/%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C-removebg-preview_1_qenllx.png';
